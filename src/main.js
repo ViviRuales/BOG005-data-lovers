0a5bab938edb693e31be6afb6c4e69c7cebfcb41
Viviana Ruales <108638400+ViviRuales@users.noreply.github.com>
2022-08-25 12:31:05 -0500

    Uso de flex para acomodar la data en el navegador

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import data from './data/pokemon/pokemon.js';
 
 /*  import data from './data/pokemon/pokemon.js';  */
 for (let index = 0; index < data.pokemon.length; index++) {
-    const section = document.createElement("section");
+    const section = document.createElement("article");
     const image = document.createElement("img");
     const title = document.createElement("h1");
     section.className = "pokemon-container";
@@ -13,7 +13,7 @@ for (let index = 0; index < data.pokemon.length; index++) {
     image.src = data.pokemon[index].img;
     section.appendChild(title);
     section.appendChild(image);
-    document.body.appendChild(section);
+    document.querySelector("#container").appendChild(section);
 }
 
 console.log(data);
