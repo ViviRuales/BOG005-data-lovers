b7b6a0f4bd5b32c84321c643ca7508b835d8c027
Viviana Ruales <108638400+ViviRuales@users.noreply.github.com>
2022-08-24 23:16:40 -0500

    Mostrar data en el navegador

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
-
+/* import { example } from './data.js'; */
 import data from './data/pokemon/pokemon.js';
 
+/*  import data from './data/pokemon/pokemon.js';  */
+for (let index = 0; index < data.pokemon.length; index++) {
+    const section = document.createElement("section");
+    const image = document.createElement("img");
+    const title = document.createElement("h1");
+    section.className = "pokemon-container";
+    image.className = "pokemon-image";
+    title.className = "pokemon-name";
+    title.textContent = data.pokemon[index].name;
+    image.src = data.pokemon[index].img;
+    section.appendChild(title);
+    section.appendChild(image);
+    document.body.appendChild(section);
+}
 
-console.log(example, data);
+console.log(data);
